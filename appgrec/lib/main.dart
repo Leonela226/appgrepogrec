c380acd74f29c44fb3ef338ac3ed3f98146f4af1
Leonela226 <anal_romeroa@unicah.edu>
2025-02-07 23:45:13 -0600

    P#2

--- appgrec/lib/main.dart
+++ appgrec/lib/main.dart
@@ -2,20 +2,24 @@ import 'package:appgrec/src/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:appgrec/src/providers/auth.dart';
-
+import 'package:appgrec/src/providers/auth.dart'; // Asegúrate de importar el proveedor
+import 'package:provider/provider.dart'; // Importa provider para usar ChangeNotifierProvider
+import 'package:intl/intl.dart';  // Asegúrate de importar este paquete
 
 void main() async {
   // Asegura que se inicialicen los widgets antes de comenzar
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  
+
   // Inicializa Firebase
   await Firebase.initializeApp();
-  
+
+  // Inicializamos la localización en español para fechas
+  Intl.defaultLocale = 'es_ES';  // Establece el idioma por defecto
+
   // Elimina la pantalla de carga después de la inicialización
   FlutterNativeSplash.remove();
-  
+
   runApp(const MyApp());
 }
 
@@ -24,10 +28,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/register',  // Establece la ruta inicial a la pantalla de registro
-      routes: appRoutes );
-
-  } 
+    return ChangeNotifierProvider(
+      create: (context) => AuthProvider(), // Aquí se agrega el proveedor
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/register',  // Establece la ruta inicial a la pantalla de registro
+        routes: appRoutes,
+      ),
+    );
+  }
 }
