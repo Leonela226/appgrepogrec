3dd93544cd4b4f9b78cf3b06b813d65ecd8361ed
Leonela226 <anal_romeroa@unicah.edu>
2025-04-08 06:36:12 -0600

    ok

---
+++ backend/Querys/insert_nappgrec.sql
@@ -0,0 +1,70 @@
+
+SHOW DATABASES;
+
+SELECT DATABASE();
+
+USE nappgrec;
+
+INSERT INTO roles (name_rol)
+VALUES ('Administrador'),
+        ('Moderador'),
+        ('cliente');
+
+INSERT INTO permissions (name_permission)
+VALUES ('Acceder al panel de administración'),
+       ('Acceder al panel de clientes'),
+       ('Crear contenido'),
+       ('Editar contenido'),
+       ('Actualizar contenido'),
+       ('Eliminar contenido'),
+       ('Editar usuarios'),
+       ('Actualizar usuarios');
+
+INSERT INTO roles_permissions (id_rol, id_permission)
+VALUES 
+    (1, 1),  -- Administrador tiene acceso al panel de administración
+    (1, 3),  -- Administrador puede crear contenido
+    (1, 4),  -- Administrador puede editar contenido
+    (1, 5),  -- Administrador puede actualizar contenido
+    (1, 6),  -- Administrador puede eliminar contenido
+    (1, 7),  -- Administrador puede editar usuarios
+    (1, 8),  -- Administrador puede actualizar usuarios
+    (2, 1),  -- Moderador tiene acceso al panel de administración
+    (2, 3),  -- Moderador puede crear contenido
+    (2, 4),  -- Moderador puede editar contenido
+    (2, 5),  -- Moderador puede actualizar contenido
+    (3, 2);  -- Cliente solo tiene acceso al panel de clientes
+
+INSERT INTO city (name_city)
+VALUES ('Choluteca'),
+       ('San Lorenzo, Valle'),
+       ('Nacaome, Valle');
+
+INSERT INTO businesses (name_business)
+VALUES ('Gasolineras UNO'),
+       ('Gasolineras TEXACO'),
+       ('Tiendas FRESSCO'),
+       ('Grec Inmobiliaria');
+
+INSERT INTO branches (name_branch, direccion_branch, id_city, id_business)
+VALUES 
+    ('Estación El Puente', 'Barrio la Cruz, contiguo a CEMCOL', 1, 1),
+    ('Estación Miramonte', 'Colonia Miramonte, boulevard Mauricio Oliva, contiguo a bodega Herco', 1, 1),
+    ('Estación Vizcaya', 'Salida a San Marcos de Colón', 1, 1),
+    ('Estación PortoSur', 'Barrio Alto Verde, boulevard Tomas Zambrano', 2, 2),
+    ('Estación Nacaome', 'Barrio San José contiguo al Parque los Garrobos', 3, 2),
+    ('Fressco Puente', 'Barrio la Cruz, contiguo a CEMCOL', 1, 3),
+    ('Fressco Miramonte', 'Colonia Miramonte, boulevard Mauricio Oliva, contiguo a bodega Herco', 1, 3),
+    ('Fressco Vizcaya', 'Salida a San Marcos de Colón', 1, 3),
+    ('Fressco PortoSur', 'Barrio Alto Verde, boulevard Tomas Zambrano', 2, 3),
+    ('Fressco Nacaome', 'Barrio San José contiguo al Parque los Garrobos', 3, 3);
+
+
+SELECT * from prizes;       
+
+
+
+
+INSERT INTO prizes (name_prize)
+VALUES ('Viaje a Roatán');
+
--- backend/routes/auth_route.js
+++ backend/routes/auth_route.js
@@ -1,6 +1,7 @@
 // routes/auth_routes.js
 const express = require("express");
 const router = express.Router();
+const { registerUser } = require("../controllers/auth_controller");
 
 // Ruta para el registro de usuarios
 router.post('/register', registerUser);
